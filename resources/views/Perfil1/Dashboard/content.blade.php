b862929bdde7d52b5731546022b227521481bf55
shadowmijlen <lenin_92_mij@hotmail.es>
2021-10-08 13:14:01 -0500

    Cambio nuevamente de títulos v2

--- resources/views/Perfil1/Dashboard/content.blade.php
+++ resources/views/Perfil1/Dashboard/content.blade.php
@@ -1,5 +1,4 @@
-<h1>Bienvenido Chupetin 123456</h1>
-<h1>Bienvenido Chupetin Trujillo 123</h1>
+<h1>Bienvenido Chupetin Trujillo 123456</h1>
 <form action="{{route('logout')}}" method="POST">
     @csrf
     <button type="submit">Cerrar Sesion</button>
